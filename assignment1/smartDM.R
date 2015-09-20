b33bfa45ce9c4e7d7db8056712ac72538555f20b
Normano64 <per.bergqwist@gmail.com>
2015-09-20 15:56:41 +0200

    Inte klar

--- assignment1/smartDM.R
+++ assignment1/smartDM.R
@@ -7,112 +7,143 @@
 ##   }
 ## }
 
+reconstruct = function(pre, goal, start) {
+
+    path = list()
+    current = goal
+    path[[1]] = current
+
+    print(pre)
+    
+    while(all(current != start)) {
+        for(i in length(pre)) {
+            if(all(pre[[i]][[2]] == current)) {
+                print(current)
+                current = pre[[i]][[1]]
+                path[[length(path) + 1]] = current
+                break
+            }
+        }
+    }
+
+    return(path)
+}
+
 astar = function(sx, sy, gx, gy, hroads, vroads) {
-  frontier = queue()
-  frontier$push(manhattan(sx, sy, gx, gy), c(x = sx, y = sy))
-  visited = list()
-  costs = list()
+    frontier = queue()
+    frontier$push(manhattan(sx, sy, gx, gy), c(x = sx, y = sy))
+    visited = list()
+    costs = list()
+    path = list()
 
-  nrow = nrow(hroads)
-  ncol = ncol(vroads)
-  costmatrix = matrix(rep(-1, nrow * ncol), ncol = ncol)
-  
-  while(TRUE) {
-    pop = frontier$pop()
-    if(all(c(pop[2], pop[3]) == c(gx, gy))) {
-      break
-    }
-    costs[[length(costs) + 1]] = pop[1]
-    visited[[length(visited) + 1]] = c(pop[2], pop[3])
-
-    current = c(cost = pop[1], pop[2], pop[3])
-
-    nodes = list()
-    paths = list()
-
-    # x is larger than 1
-    if(current['x'] > 1) {
-      nodes[[length(nodes) + 1]] = c(current['x'] - 1, current['y'])
-      paths = append(paths, hroads[current['y'], current['x'] - 1])
-    }
-    # x is smaller than ncol
-    if(current['x'] < ncol) {
-      nodes[[length(nodes) + 1]] = c(current['x'] + 1, current['y'])
-      paths = append(paths, hroads[current['y'], current['x'] + 1])
-    }
-    # y is larger than 1
-    if(current['y'] > 1) {
-      nodes[[length(nodes) + 1]] = c(current['x'], current['y'] - 1)
-      paths = append(paths, hroads[current['y'] - 1, current['x']])
-    }
-    # y is smaller than nrow
-    if(current['y'] < nrow) {
-      nodes[[length(nodes) + 1]] = c(current['x'], current['y'] + 1)
-      paths = append(paths, hroads[current['y'] + 1, current['x']])
-    }
-    for(i in 1:length(nodes)) {
-      cost = (current['cost'] + manhattan(nodes[[i]]['x'], nodes[[i]]['y'], gx, gy) - manhattan(current['x'], current['y'], gx, gy) + paths[[i]])
-      if(frontier$exist(c(nodes[[i]]['x'], nodes[[i]]['y']))) {
-        if(frontier$peek(c(nodes[[i]]['x'], nodes[[i]]['y'])) > cost) {
-          frontier$update(cost, c(nodes[[i]]['x'], nodes[[i]]['y']))
+    nrow = nrow(hroads)
+    ncol = ncol(vroads)
+    costmatrix = matrix(rep(-1, nrow * ncol), ncol = ncol)
+    
+    while(TRUE) {
+        pop = frontier$pop()
+        if(all(c(pop[2], pop[3]) == c(gx, gy))) {
+            return(reconstruct(path, c(gx, gy), c(sx, sy)))
+            ## (list(visited, costs))
         }
-      } else {
-        frontier$push(cost, c(nodes[[i]]['x'], nodes[[i]]['y']))
-      }
+        costs[[length(costs) + 1]] = pop[1]
+        visited[[length(visited) + 1]] = c(pop[2], pop[3])
+
+        current = c(cost = pop[1], pop[2], pop[3])
+
+        nodes = list()
+        paths = list()
+
+        ## print(matrix(c("Costs", costs), nrow = 1), row.names = FALSE)
+        ## print(matrix(visited, nrow = 1), row.names = FALSE)
+        ## print(current, row.names = FALSE)
+        ## Sys.sleep(5)
+        
+        ## x is larger than 1
+        if(current['x'] > 1) {
+            nodes[[length(nodes) + 1]] = c(current['x'] - 1, current['y'])
+            paths = append(paths, vroads[current['y'], current['x'] - 1])
+        }
+        ## x is smaller than ncol
+        if(current['x'] < ncol) {
+            nodes[[length(nodes) + 1]] = c(current['x'] + 1, current['y'])
+            paths = append(paths, vroads[current['y'], current['x'] + 1])
+        }
+        ## y is larger than 1
+        if(current['y'] > 1) {
+            nodes[[length(nodes) + 1]] = c(current['x'], current['y'] - 1)
+            print(matrix(current['x'],current['y']))
+            paths = append(paths, hroads[current['y'] - 1, current['x']])
+        }
+        ## y is smaller than nrow
+        if(current['y'] < nrow) {
+            nodes[[length(nodes) + 1]] = c(current['x'], current['y'] + 1)
+            paths = append(paths, hroads[current['y'] + 1, current['x']])
+        }
+        for(i in 1:length(nodes)) {
+            cost = (current['cost'] + manhattan(nodes[[i]]['x'], nodes[[i]]['y'], gx, gy) - manhattan(current['x'], current['y'], gx, gy) + paths[[i]])
+            if(frontier$exist(c(nodes[[i]]['x'], nodes[[i]]['y']))) {
+                if(frontier$peek(c(nodes[[i]]['x'], nodes[[i]]['y'])) > cost) {
+                    frontier$update(cost, c(nodes[[i]]['x'], nodes[[i]]['y']))
+                }
+            } else {
+                frontier$push(cost, c(nodes[[i]]['x'], nodes[[i]]['y']))
+                path[[length(path) + 1]] = list(c(current['x'], current['y']), c(nodes[[i]]['x'], nodes[[i]]['y']))
+            }
+        }
     }
-  }
-  return (c(visited, costs))
 }
 
+## Manhattan distance from (c1, c2) to (g1, g2)
 manhattan = function(c1, c2, g1, g2) {
-  return (abs(c1 - g1) + abs(c2 - g2))
+    return (abs(c1 - g1) + abs(c2 - g2))
 }
 
-# modified priority queue from
-# http://rosettacode.org/wiki/Priority_queue#R
+## modified priority queue from
+## http://rosettacode.org/wiki/Priority_queue#R
 queue = function() {
-  keys <<- values <<- NULL
-  # pushes value with weight as key
-  push = function(key, value) {
-    temp <- c(keys, key)
-    ord <- order(temp)
-    keys <<- temp[ord]
-    values <<- c(values, list(value))[ord]
-  }
-  # pops lowest weight
-  pop = function() {
-    head <- c(keys[[1]], values[[1]])
-    keys <<- keys[-1]
-    values <<- values[-1]
-    return (head)
-  }
-  # updates value with new key
-  update = function(key, value) {
-    index <- seek(value)
-    keys <<- keys[-index]
-    values <<- values[-index]
-    push(key, value)
-  }
-  # returns index of value, private function
-  seek = function(value) {
-    index <- NULL
-    if(!empty()){
-      for(i in 1:size()) {
-        if(all(values[[i]] == value)) {
-          index <- i
-          break
-        }
-      }
+    keys <<- values <<- NULL
+    ## pushes value with weight as key
+    push = function(key, value) {
+        temp <- c(keys, key)
+        ord <- order(temp)
+        keys <<- temp[ord]
+        values <<- c(values, list(value))[ord]
     }
-    return (index)
-  }
-  # look up weight of value
-  peek = function(value) keys[seek(value)]
-  # TRUE if exists, else FALSE
-  exist = function(value) !is.null(seek(value))
-  # returns length of the queue
-  size = function() length(keys)
-  # TRUE if queue is empty, else FALSE
-  empty = function() length(keys) == 0
-  list(push = push, pop = pop, update = update, peek = peek, exist = exist, size = size, empty = empty)
+    ## pops lowest weight
+    pop = function() {
+        head <- c(keys[[1]], values[[1]])
+        keys <<- keys[-1]
+        values <<- values[-1]
+        return (head)
+    }
+    ## updates value with new key
+    update = function(key, value) {
+        index <- seek(value)
+        keys <<- keys[-index]
+        values <<- values[-index]
+        push(key, value)
+    }
+    ## returns index of value, private function
+    seek = function(value) {
+        index <- NULL
+        if(!empty()){
+            for(i in 1:size()) {
+                if(all(values[[i]] == value)) {
+                    index <- i
+                    break
+                }
+            }
+        }
+        return (index)
+    }
+    ## look up weight of value
+    peek = function(value) keys[seek(value)]
+    ## TRUE if exists, else FALSE
+    exist = function(value) !is.null(seek(value))
+    ## returns length of the queue
+    size = function() length(keys)
+    ## TRUE if queue is empty, else FALSE
+    empty = function() length(keys) == 0
+    list(push = push, pop = pop, update = update, peek = peek, exist = exist, size = size, empty = empty)
 }
